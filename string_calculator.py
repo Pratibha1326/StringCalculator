a6272f3055e233deb4ef82296b18a4bb3e667d82
Pratibha <partibhakumari7@gmail.com>
2025-01-21 17:29:02 +0530

    implimented logic for sum of 2 numbers

--- string_calculator.py
+++ string_calculator.py
@@ -2,5 +2,10 @@ class StringCalculator:
     def add(self, numbers: str)-> int:
         if numbers == "":
            return 0
+        
+        if "," in numbers:
+            
+            returns int(numbers[0]) + int(numbers[1])
+
         return int(numbers)
         
