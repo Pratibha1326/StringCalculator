f14cb212da729b0c188da94f403e488db0f1aed5
Pratibha <partibhakumari7@gmail.com>
2025-01-21 17:06:32 +0530

    added test cases for single and two numbers

--- test_string_calculator.py
+++ test_string_calculator.py
@@ -6,3 +6,13 @@ from string_calculator import StringCalculator
 def test_empty_string():
     calculator = StringCalculator()
     assert calculator.add("") == 0
+
+# Test case for single number
+def test_single_number():
+    calculator = StringCalculator()
+    assert calculator.add("1") == 1
+# Test case for two numbers, should return some of 2 numbers.
+def test_sum_two_number():
+    calculator = StringCalculator()
+    assert calculator.add("1, 2") == 3
+
