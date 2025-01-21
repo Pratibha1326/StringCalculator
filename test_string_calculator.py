15b2373e036a9797a2843dde132c90988f3d1842
Pratibha <partibhakumari7@gmail.com>
2025-01-21 17:13:35 +0530

    test case for unknown numbers and new line between numbers

--- test_string_calculator.py
+++ test_string_calculator.py
@@ -11,8 +11,20 @@ def test_empty_string():
 def test_single_number():
     calculator = StringCalculator()
     assert calculator.add("1") == 1
+
 # Test case for two numbers, should return some of 2 numbers.
 def test_sum_two_number():
     calculator = StringCalculator()
     assert calculator.add("1, 2") == 3
+
+#  Test case for multiple numbers
+def test_sum_unknown_number():
+    calculator = StringCalculator()
+    assert calculator.add("1, 2, 3, 5, 8, 9") == 28
+    
+# Test case  for new line in between numbers.
+def test_new_line_between_numbers():
+    calculator = StringCalculator()
+    assert calculator.add("1\n2,3") == 6
+
 
